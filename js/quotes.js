ae603d31d14b4f1a8785ce2c3f9d851b6f5951a5
Maggie Bignell <mbignell@users.noreply.github.com>
2017-11-04 17:39:25 -0700

    Added files

---
+++ js/quotes.js
@@ -0,0 +1,59 @@
+var quotes = [
+
+  {
+    quote: "Look, if you think we&rsquo;re wrong, then I respect that. But if you think we&rsquo;re right and you won&rsquo;t speak up because you can't be bothered, then God, Jed, I don't even want to know you.",
+    source: "Mrs. Langingham",
+    citation: "Two Cathedrals",
+    date: "2001",
+    bgcolor: "#4a569d",
+    bggradient: "linear-gradient(315deg, #dc2424, #4a569d)"
+  },
+
+  {
+    quote: "It&rsquo;s not the battles we lose that bother me, it&rsquo;s the ones we don&rsquo;t suit up for.",
+    source: "Toby Ziegler",
+    citation: "Let Bartlet Be Bartlet",
+    date: "2000",
+    bgcolor: "#480048",
+    bggradient: "linear-gradient(315deg, #c04848, #480048)"
+  },
+
+  {
+    quote: "The theme of this year&rsquo;s event is, &lsquo;learning is delightful and delicious&rsquo; - as, by the way, am I.",
+    source: "C.J. Cregg",
+    citation: "Let Barlet Be Bartlet",
+    date: "2000",
+    tags: ["C.J."],
+    bgcolor: "#d76d77",
+    bggradient: "linear-gradient(135deg, #3a1c71, #d76d77, #ffaf7b)"
+  },
+
+  {
+    quote: "What&rsquo;s next?",
+    source: "Jed Bartlet",
+    tags: ["Catchphrase", "The President"],
+    bgcolor: "#485563",
+    bggradient: "linear-gradient(135deg, #485563, #29323c)"
+  },
+
+  {
+    quote: "We need someone perceived by the American people as irresponsible, untrustworthy, patisan, ambitious, and thirsty for the limelight. Am I crazy, or is this not a job for the House of Representatives?",
+    source: "C.J. Cregg",
+    citation: "Ways and Means",
+    date: "2001",
+    tags: ["C.J.", "Government","More", "Tags"],
+    bgcolor: "#191654",
+    bggradient: "linear-gradient(315deg, #43c6ac, #191654)"
+  },
+
+  {
+    quote: "People think campaigns are about two competing answers to the same question. They&rsquo;re not. They&rsquo;re a fight over the question itself.",
+    source: "Josh Lyman",
+    citation: "The Mommy Problem",
+    date: "2005",
+    tags: ["Josh", "Politics"],
+    bgcolor: "#44a08d",
+    bggradient: "linear-gradient(315deg, #44a08d, #093637);"
+  }
+
+];
---
+++ js/script.js
@@ -0,0 +1,98 @@
+// event listener to respond to "Show another quote" button clicks (swapped for mousedown to fix the button not responding)
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("mousedown", buttonClick, false);
+
+// Sets global variable to keep track of current quote
+var currentQuoteIndex = 0;
+
+// Function : Return random quote from array
+function getRandomQuote() {
+  // Generates a random number that we'll use as an Index on the array
+  var randomIndex = Math.floor(Math.random() * (quotes.length));
+
+  // If it's the same as the current quote, get a new number
+  while (currentQuoteIndex == randomIndex) {
+    randomIndex = Math.floor(Math.random() * (quotes.length));
+  }
+
+  // Set Current Index to New Index
+  currentQuoteIndex = randomIndex;
+
+  // Return the quote object
+  return quotes[randomIndex];
+};
+
+// Function : Creates HTML block and prints to page
+function printQuote() {
+  // Calls getRandomQuote and stores object in a variable
+  var currentQuote = getRandomQuote();
+
+  // Begins to construct the HTML we'll add in
+  var quoteHTML = "<p class=\"quote\">" + currentQuote.quote + "</p>";
+
+  // Opens source paragraph and adds source
+  quoteHTML += '<p class="source">' + currentQuote.source;
+
+  // Add Citation if it exists
+  if (currentQuote.citation) {
+    quoteHTML += '<span class="citation">' + currentQuote.citation + '</span>';
+  };
+
+  // Add Date if it exists
+  if (currentQuote.date) {
+    quoteHTML += '<span class="year">' + currentQuote.date + '</span>';
+  };
+
+  // Closes source paragraph whether or not citations and dates were added
+  // Opens tag div
+  quoteHTML += '</p> <div class="tags">';
+
+  // If there are tags in the object, add each one in the array as a link
+  if (currentQuote.tags) {
+    var currentTags = currentQuote.tags;
+    for(i in currentTags) {
+      var tagHTML = '<a href="#whatever" class="tag">' + currentTags[i] + '</a>';
+      quoteHTML += tagHTML;
+    }
+  }
+
+  // Closes Tags div
+  quoteHTML += '</div>';
+
+  // Change background and text colors based on the colors associated to the object literal.
+  // First line specifically changes the root css variable, and second line the body's background-image (a gradient);
+  document.documentElement.style.setProperty('--main-color', currentQuote.bgcolor);
+  var string = 'background-image: ' + currentQuote.bggradient + '; background-image:  -webkit-' + currentQuote.bggradient + ';';
+  document.getElementsByTagName('body')[0].style.cssText = string;
+
+  // Displays created HTML on page
+  document.getElementById('quote-box').innerHTML = quoteHTML;
+
+  return;
+};
+
+// When the page load, it selects a random quote to display
+window.onload = printQuote();
+
+// Create interval to let quotes change every 10 seconds.
+var interval;
+interval = setInterval(function(){
+  printQuote();
+},10000);
+
+// Adds restarting the interval to the original PrintQuote button click
+function buttonClick() {
+  printQuote();
+  // Clears JavaScript interval
+  clearInterval(interval);
+  // Restarts CSS animation to match button click
+  var loadingBar = document.getElementById('loadingQuote')
+  loadingBar.classList.remove("loadingQuoteAnimate");
+  interval = setInterval(function(){
+    loadingBar.classList.add("loadingQuoteAnimate");
+  },1);
+  // Restarts JavaScript Interval
+  interval = setInterval(function(){
+    printQuote();
+  },10000);
+};
